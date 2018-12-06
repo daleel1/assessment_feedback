f04ad43be4d56d4d8abc33c0e46a109dca7a3ac1
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-06 20:22:38 +0000

    create a form

--- frontend/src/Componants/Form.js
+++ frontend/src/Componants/Form.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import DropDownT from "./DropDownList";
+import DetailForm from "./Fields";
+
 
 
 export default class Form extends Component {
@@ -7,6 +9,7 @@ export default class Form extends Component {
         return (
             <div>
                 <DropDownT />
+                <DetailForm/>
 
             </div>
         );
