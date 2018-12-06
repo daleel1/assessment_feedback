c902ece01b917082d6a90b5df6e7f98b829d0530
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-06 20:20:56 +0000

    create dropdown list

---
+++ frontend/src/Componants/DropDownList.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class DropDownT extends React.Component {
+    state = {
+        values: []
+    };
+    componentDidMount() {
+        fetch("https://api.myjson.com/bins/8oy5q")
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ values: data });
+                console.log(data);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="drop-down mt-5">
+                    <p>
+                        Code to fetch the list of the options in the dropdown, which runs
+                        when the page loads
+          </p>
+                    <div className="container">
+                        <select className="custom-select" id="inputGroupSelect01">
+                            {this.state.values.map(obj => {
+                                return <option value={obj.id}>{obj.name}</option>;
+                            })}
+                        </select>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DropDownT;
--- frontend/src/Componants/Form.js
+++ frontend/src/Componants/Form.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import DropDownT from "./DropDownList";
 
 
 export default class Form extends Component {
     render() {
         return (
             <div>
-               hi
+                <DropDownT />
+
             </div>
         );
     }
