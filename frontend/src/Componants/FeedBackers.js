da473b2fef468ad5244e126bc2c550415d247196
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-30 23:20:47 +0000

    list of feedbackers

---
+++ frontend/src/Componants/FeedBackers.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class FeedBackers extends React.Component {
+  state = {
+    values: []
+  };
+  componentDidMount() {
+    fetch("https://api.myjson.com/bins/8oy5q")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ values: data });
+        console.log(data);
+      });
+  }
+  /*when input changes,call addValues({
+          inputKey: inputValue,
+          key2: value2
+      })*/
+
+  render() {
+    return (
+      <div>
+        <div className="drop-down mt-5">
+          <p>
+            1- Please tell us who you are?*
+          </p>
+          <div className="container">
+            <select
+              className="custom-select mt-3"
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              <option>Select Your Name ..</option>
+              {this.state.values.map(obj => {
+                return (
+                  <option key={obj.id} value={obj.id}>
+                    {obj.name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FeedBackers;
