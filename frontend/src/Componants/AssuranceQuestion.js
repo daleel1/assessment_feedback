77fe1bf44551ee8f291fceba45312489876544a9
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-31 05:36:25 +0000

    adding assurance Question

---
+++ frontend/src/Componants/AssuranceQuestion.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class AssuranceQuestion extends React.Component {
+  state = {
+    values: []
+  };
+  componentDidMount() {
+    fetch("https://api.myjson.com/bins/8oy5q")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ values: data });
+        console.log(data);
+      });
+  }
+  /*when input changes,call addValues({
+              inputKey: inputValue,
+              key2: value2
+          })*/
+
+  render() {
+    return (
+      <div>
+        <div className="drop-down mt-5">
+          <div className="container">
+            <select
+              className="custom-select mt-3"
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              <option>
+                Have you worked with Ahmed enough to give feedback on this? *
+              </option>
+              <option value={"Yes"}>Yes</option>
+              <option value={"No"}>No</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AssuranceQuestion;
