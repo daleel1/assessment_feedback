6357ab97b9b0f8e8560dc4b627f1a92bffc7b87c
Daleel Hagy <daleel.n.hagy@gmail.com>
2019-01-06 17:52:08 +0000

    restyling sub soft skills

--- frontend/src/Componants/SubSoftSkills.js
+++ frontend/src/Componants/SubSoftSkills.js
@@ -21,16 +21,16 @@ export default class SubSoftSkills extends React.Component {
         <h4 className="text-center mb-3 mt-5">
           please select a habit to give feedback on
         </h4>
-        <RadioGroup className="font-weight-bold" onChange={this.onChange}>
-          <RadioButton value="1">
+        <RadioGroup className="darkText" onChange={this.onChange}>
+          <RadioButton iconSize={20} value="1">
             I update others every day on progress with tasks
           </RadioButton>
-          <RadioButton value="2">
+          <RadioButton iconSize={20} value="2">
             I reply as sson as possible to individual or group communication and
             I let people know I've received their messages(e.g. thumbs up is
             Slack)
           </RadioButton>
-          <RadioButton value="3">
+          <RadioButton iconSize={20} value="3">
             I ask questions when I don't understand or am unsure about what to
             do
           </RadioButton>
