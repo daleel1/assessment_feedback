8b9c8a5d85bc48a9075c4ae7a33fe1134551384d
Daleel Hagy <daleel.n.hagy@gmail.com>
2019-01-06 17:31:14 +0000

    update soft skills styling

--- frontend/src/Componants/SoftSkills.js
+++ frontend/src/Componants/SoftSkills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { RadioGroup, RadioButton } from "react-radio-buttons";
+import "../Styling/Styling.css";
 
 export default class SoftSkills extends React.Component {
   onChange = () => {
@@ -16,18 +17,22 @@ export default class SoftSkills extends React.Component {
   };
 
   render() {
-    return (
-      <div className="container mt-2">
+    return <div className="container mt-2">
         <h4 className="text-center mb-3 mt-5">
           Please select a topic area to give feedback on
         </h4>
-        <RadioGroup className="font-weight-bold" onChange={this.onChange}>
-          <RadioButton value="1">participating in my class</RadioButton>
-          <RadioButton value="2">supporting my class</RadioButton>
-          <RadioButton value="3">solving problems</RadioButton>
-          <RadioButton value="4">taking responsibility</RadioButton>
+        <RadioGroup className="darkText" onChange={this.onChange}>
+          <RadioButton iconSize={20} value="1">
+            participating in my class
+          </RadioButton>
+          <RadioButton iconSize={20}  value="2">
+            supporting my class
+          </RadioButton>
+        <RadioButton iconSize={20} className="text-dark" value="3">
+            solving problems
+          </RadioButton>
+        <RadioButton iconSize={20} value="4">taking responsibility</RadioButton>
         </RadioGroup>
-      </div>
-    );
+      </div>;
   }
 }
